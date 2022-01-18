aedb4df1d21fa70a6e553d6f7a679e180e97b7d0
wilson2.wang@loreal.com <wilson2.wang@loreal.com>
2022-01-18 23:22:08 +0800

    uploaded all dbt related changes

--- _win_deploy.bat
+++ _win_deploy.bat
@@ -1,1 +1,2 @@
-gsutil rsync dags gs://asia-east2-airflow-prod-6c35af45-bucket/dags
+call gsutil -m rsync -r -d dags gs://asia-east2-airflow-prod-6c35af45-bucket/dags
+call gsutil -m rsync -r -d data gs://asia-east2-airflow-prod-6c35af45-bucket/data
